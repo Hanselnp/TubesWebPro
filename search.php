2b066f9175be196f4038319e9cbaaefd5f059840
hanselnp <www.hanselnp99@gmail.com>
2018-03-12 01:08:36 +0700

    Update on search result page.

---
+++ search.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+	<title>Premicom</title>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/src.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+      <div class="container-fluid">
+        <div class="col-xs-9 header-container">
+          <div class="pull-left">
+            <div class="logo">
+              <img src="#">
+            </div>
+            <div class="menu">
+              <ul class="nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Rilisan Harian</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Genre</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Beli Komik</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="pull-right">
+            <div class="search">
+              <input placeholder="Cari komik"><div  class="search-button"><i class="fas fa-search"></i></div>
+            </div>
+            <div class="login">
+              <a href="#">Login</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <div class="container outer">
+      <h1>Hasil Pencarian :</h1>
+      <div class="row">
+        <div class="result col-8 ">
+          <div class="row">
+            <div class="col-12 col-md-2">
+                  <div class="title">
+                    <img src="images/test.jpeg">
+                  </div>
+            </div>
+              <div class="col-12 col-md-8 container box">
+                <div class="title col-12">
+                  <a href="#"><h3>Fuuka</h3></a>
+                </div>
+                <div class="container col-12">
+                  Description :
+                  Fuuka is a sequel to Seo Kouji's popular manga Suzuka. The story follows the life of Yuu Haruna, who recently moved into Tokyo with his sisters after their father is forced to transfer overseas on ...
+                </div>
+                <div class="genre col-12 ">
+                      Genre :
+                      <a href="#">Ecchi</a>
+                      <a href="#">Yuri</a>
+                      <a href="#">Romance</a>
+                      <a href="#">Drama</a>
+
+                      <div class="r8m8">
+                        Rating :
+                        <img src="images/4_star.png">
+                      </div>
+                </div>
+              </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="row">
+        <div class="result col-8 ">
+          <div class="row">
+            <div class="col-12 col-md-2">
+                  <div class="title">
+                    <img src="images/test.jpeg">
+                  </div>
+            </div>
+              <div class="col-12 col-md-8 container box">
+                <div class="title col-12">
+                  <a href="#"><h3>Fuuka</h3></a>
+                </div>
+                <div class="container col-12">
+                  Description :
+                  Fuuka is a sequel to Seo Kouji's popular manga Suzuka. The story follows the life of Yuu Haruna, who recently moved into Tokyo with his sisters after their father is forced to transfer overseas on ...
+                </div>
+                <div class="genre col-12 ">
+                      Genre :
+                      <a href="#">Ecchi</a>
+                      <a href="#">Yuri</a>
+                      <a href="#">Romance</a>
+                      <a href="#">Drama</a>
+
+                      <div class="r8m8">
+                        Rating :
+                        <img src="images/4_star.png">
+                      </div>
+                </div>
+              </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="row">
+        <div class="result col-8 ">
+          <div class="row">
+            <div class="col-12 col-md-2">
+                  <div class="title">
+                    <img src="images/test.jpeg">
+                  </div>
+            </div>
+              <div class="col-12 col-md-8 container box">
+                <div class="title col-12">
+                  <a href="#"><h3>Fuuka</h3></a>
+                </div>
+                <div class="container col-12">
+                  Description :
+                  Fuuka is a sequel to Seo Kouji's popular manga Suzuka. The story follows the life of Yuu Haruna, who recently moved into Tokyo with his sisters after their father is forced to transfer overseas on ...
+                </div>
+                <div class="genre col-12 ">
+                      Genre :
+                      <a href="#">Ecchi</a>
+                      <a href="#">Yuri</a>
+                      <a href="#">Romance</a>
+                      <a href="#">Drama</a>
+
+                      <div class="r8m8">
+                        Rating :
+                        <img src="images/4_star.png">
+                      </div>
+                </div>
+              </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="row">
+        <div class="result col-8 ">
+          <div class="row">
+            <div class="col-12 col-md-2">
+                  <div class="title">
+                    <img src="images/test.jpeg">
+                  </div>
+            </div>
+              <div class="col-12 col-md-8 container box">
+                <div class="title col-12">
+                  <a href="#"><h3>Fuuka</h3></a>
+                </div>
+                <div class="container col-12">
+                  Description :
+                  Fuuka is a sequel to Seo Kouji's popular manga Suzuka. The story follows the life of Yuu Haruna, who recently moved into Tokyo with his sisters after their father is forced to transfer overseas on ...
+                </div>
+                <div class="genre col-12 ">
+                      Genre :
+                      <a href="#">Ecchi</a>
+                      <a href="#">Yuri</a>
+                      <a href="#">Romance</a>
+                      <a href="#">Drama</a>
+
+                      <div class="r8m8">
+                        Rating :
+                        <img src="images/4_star.png">
+                      </div>
+                </div>
+              </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+      </div>
+    </div>
+
+  <!-- Javascript & Jquery Init -->
+  <script src="js_src/src.js">
+	</script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js">
+	</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
+	</script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js">
+	</script>
+  <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+</body>
+</html>
