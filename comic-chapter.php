5ef026127d255ddec5134da356706811f5930f7e
Muhammad Satrio Wibowo <bowo.6450@gmail.com>
2018-03-14 01:34:22 +0700

    Updates and Cleaning

---
+++ comic-chapter.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+	<title>Premicom</title>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+  <link href="css/src.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+      <div class="container-fluid">
+        <div class="col-xs-9 header-container">
+          <div class="pull-left">
+            <div class="logo">
+              <img sstyle="background-image: url('images/dummy.jpg')"rc="#">
+            </div>
+            <div class="menu">
+              <ul class="nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Rilisan Harian</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Genre</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Beli Komik</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="pull-right">
+            <div class="search">
+              <input placeholder="Cari komik"><div class="search-button"><i class="fas fa-search"></i></div>
+            </div>
+            <div class="login">
+              <a onclick="" href="#">Login</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="comic-title-container" style="background-image: url('images/dummy.jpg')">
+      <div class="col-12">
+        <div class="row">
+          <div class="col-8 text-center mx-auto">
+            <div class="comic-title">
+              This is the Title
+            </div>
+            <div class="comic-creator">
+              Hideyoshi Tanaka
+            </div>
+            <div class="comic-genre">
+              Genre :
+              <a href="#">Ecchi</a>
+              <a href="#">Yuri</a>
+              <a href="#">Romance</a>
+              <a href="#">Drama</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+		<div class="container comic-episodes-container">
+      <div class="row">
+        <div class="col-12 col-md-8">
+          <div class="comic-episodes">
+            <div class="row">
+              <div class="col-2 comic-episodes-thumb">
+                <img src="images/thumbnail.jpg"/>
+              </div>
+              <div class="col-6 comic-episodes-title">
+                <p>This is Title</p>
+              </div>
+              <div class="col-4 comic-episodes-timestamp">
+                <div class="row no-gutters">
+                  <div class="col-9">
+                    <p>Mar 18, 2018</p>
+                  </div>
+                  <div class="col-3">
+                    #10
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="comic-episodes">
+            <div class="row">
+              <div class="col-2 comic-episodes-thumb">
+                <img src="images/thumbnail.jpg"/>
+              </div>
+              <div class="col-6 comic-episodes-title">
+                <p>This is Title</p>
+              </div>
+              <div class="col-4 comic-episodes-timestamp">
+                <div class="row no-gutters">
+                  <div class="col-9">
+                    <p>Mar 18, 2018</p>
+                  </div>
+                  <div class="col-3">
+                    #9
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="comic-episodes">
+            <div class="row">
+              <div class="col-2 comic-episodes-thumb">
+                <img src="images/thumbnail.jpg"/>
+              </div>
+              <div class="col-6 comic-episodes-title">
+                <p>This is Title</p>
+              </div>
+              <div class="col-4 comic-episodes-timestamp">
+                <div class="row no-gutters">
+                  <div class="col-9">
+                    <p>Mar 18, 2018</p>
+                  </div>
+                  <div class="col-3">
+                    #8
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4">
+            adasdasd
+        </div>
+      </div>
+    </div>
+
+    <div class="container comics-recommendation">
+      <div class="row comics-recommendation-head">
+        Mungkin anda juga berminat membaca
+      </div>
+      <div class="row mt-2 mb-5">
+        <div class="col-12 comics-recommendation-body">
+          <div class="row">
+            <div class="col-4">
+              aaa
+            </div>
+            <div class="col-4">
+              bbb
+            </div>
+            <div class="col-4">
+              ccc
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  <!-- Javascript & Jquery Init -->
+	<script src="js_src/src.js">
+	</script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js">
+	</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
+	</script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js">
+	</script>
+  <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+</body>
+</html>
