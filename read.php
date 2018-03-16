b9c7c4296307e8d974a4dcc9f8d404cdb340015d
Muhammad Satrio Wibowo <bowo.6450@gmail.com>
2018-03-16 18:16:30 +0700

    Completed CRUD

--- read.php
+++ read.php
@@ -42,69 +42,91 @@
 			</div>
 			<div class="row">
 				<div class="col-12 col-md-8">
-					<div class="row input-comment">
-						<div class="col">
-							<textarea placeholder="Masukkan komentar disini..."></textarea>
+					<form method="get" id="insertComments">
+						<div class="row no-gutters input-comment">
+							<div class="col">
+								<textarea placeholder="Masukkan komentar disini..."></textarea>
+							</div>
+							<div class="col-4 ml-2">
+								<button type="submit">Kirim Komentar</button>
+							</div>
 						</div>
-						<div class="col-4">
-							<button>ABC</button>
-						</div>
-					</div>
+					</form>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-12 col-md-8">
-					<div class="comment1">
-				    <img src="profile/becak.JPG" style="margin-top: 10px;margin-left: 55px;width: 50px;height: 50px; border-radius: 5px;">
-				    <div class="nameprofile" style="margin-left: 110px;margin-top: -55px;color: blue; font-family: roboto">
-				      TukangBecak
-				      <div style="color: black;display: inline-block;">
-				        • 3 hari yang lalu
-				      </div>
-				    </div>
-				    <div class="pesan" style="margin-left: 120px; margin-top: -5px;">
-				      yang begini nih emang mantep dah, ditunggu kelanjutannyaa ...
-				    </div>
-				    <div class="likes" style="margin-left: 110px; margin-top: 0px">
-				      <i class="far fa-thumbs-up"></i> 18<i class="far fa-thumbs-down" style="margin-left: 7px"></i> 7 •Balas •Bagikan ›
-				    </div>
-				  </div>
-				  <div class="comment2">
-				    <img src="profile/tukangroti.jpg" style="margin-top: 10px;margin-left: 55px;width: 50px;height: 50px; border-radius: 5px;">
-				    <div class="nameprofile" style="margin-left: 110px;margin-top: -55px;color: blue; font-family: roboto">
-				      TukangRoti22
-				      <div style="color: black;display: inline-block;">
-				        • 5 hari yang lalu
-				      </div>
-				    </div>
-				    <div class="pesan" style="margin-left: 120px; margin-top: -5px;">
-				      wih keren nih, masukin daftar favorit laahh!!
-				    </div>
-				    <div class="likes" style="margin-left: 110px; margin-top: 0px">
-				      <i class="far fa-thumbs-up"></i> 21<i class="far fa-thumbs-down" style="margin-left: 7px"></i> 4 •Balas •Bagikan ›
-				    </div>
-				  </div>
-				  <div class="comment3">
-				    <img src="profile/odongodong.jpg" style="margin-top: 10px;margin-left: 55px;width: 50px;height: 50px; border-radius: 5px;">
-				    <div class="nameprofile" style="margin-left: 110px;margin-top: -55px;color: blue; font-family: roboto">
-				      DongodongStanding
-				      <div style="color: black;display: inline-block;">
-				        • 6 hari yang lalu
-				      </div>
-				    </div>
-				    <div class="pesan" style="margin-left: 120px; margin-top: -5px;">
-				      kalo ada op nya nanti di setel di odong odong saya bole?
-				    </div>
-				    <div class="likes" style="margin-left: 110px; margin-top: 0px">
-				      <i class="far fa-thumbs-up"></i> 29<i class="far fa-thumbs-down" style="margin-left: 7px"></i> 10 •Balas •Bagikan ›
-				    </div>
-				  </div>
-				</div>
-			</div>
-				<div id="discussion">
-
+				<div class="col-12 col-md-8 komentar">
+					Wah! Sepertinya belum ada komentar nih :(
+					Jadilah yang pertama berkomentar disini!
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
+
+<script>
+	var commentCount = 0;
+
+	$("#insertComments").on("submit", function(e) {
+		e.preventDefault();
+		var name = "Anonim";
+		var komentar = $("#insertComments textarea").val();
+		$("#insertComments textarea").val("");
+
+		if (commentCount != 0) {
+			$(".komentar").append(`
+				<div class="row no-gutters mt-2 comment-item">
+					<div class="col-1">
+						<img src="images/profile-square-extra-small.png" style="width: 100%; height: 100%; border-radius: 5px;">
+					</div>
+					<div class="col-9 ml-2">
+						<div class="nameprofile">
+							<b>`+name+`</b>
+						</div>
+						`+komentar+`
+					</div>
+					<div class="col-1 ml-1">
+						<i class="far fa-trash-alt delete"></i>
+					</div>
+				</div>
+			`);
+		} else {
+			$(".komentar").html(`
+				<div class="row no-gutters mt-2 comment-item">
+					<div class="col-1">
+						<img src="images/profile-square-extra-small.png" style="width: 100%; height: 100%; border-radius: 5px;">
+					</div>
+					<div class="col-9 ml-2">
+						<div class="nameprofile">
+							<b>`+name+`</b>
+						</div>
+						`+komentar+`
+					</div>
+					<div class="col-1 ml-1">
+						<i class="far fa-trash-alt delete"></i>
+					</div>
+				</div>
+			`);
+		}
+		commentCount++;
+		setTimeout(function() {
+			refreshBinding()
+		}, 10);
+	});
+
+	function refreshBinding() {
+		$(".delete").unbind("click");
+		$.each($(".delete"), function(index, val) {
+			$(this).on("click", function(vax) {
+				$(this).parent().parent().remove();
+				commentCount--;
+				if (commentCount == 0) {
+					$(".komentar").html(`
+						Wah! Sepertinya belum ada komentar nih :(
+						Jadilah yang pertama berkomentar disini!
+					`);
+				};
+			})
+		});
+	}
+</script>
