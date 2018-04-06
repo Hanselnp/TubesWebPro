4018f5d3563faaad8edae31ba6cf18db99dbe1aa
Muhammad Satrio Wibowo <bowo.sma6@gmail.com>
2018-04-07 02:03:31 +0700

    Major UI Update

---
+++ application/views/javascript-loader.php
@@ -0,0 +1,9 @@
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
+</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js">
+</script>
+<script src="resources/js/fontawesome.js">
+</script>
+<script src="resources/js/main.js">
+</script>
