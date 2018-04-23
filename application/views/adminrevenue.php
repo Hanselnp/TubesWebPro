83931cfe8da3e35592d39e71e3781b8a72f02f8e
hanselnp <www.hanselnp99@gmail.com>
2018-04-23 23:32:48 +0700

    Update admin page

---
+++ application/views/adminrevenue.php
@@ -0,0 +1,17 @@
+<?php include ('header.php'); ?>
+  <body>
+    <?php include ('admin_navbar.php'); ?>
+    <div class="wrapper">
+      <?php include ('sidebar.php'); ?>
+      <div class="col-12 col-sm-10">
+        <div class="container" style="padding-bottom: 30px;">
+          <div class="row">
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php include ('javascript-loader.php'); ?>
+  </body>
+</html>
+<?php include('footer.php'); ?>
